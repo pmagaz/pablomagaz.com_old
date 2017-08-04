288289406dc3520293bb18b36f201a0035e02b26
Pablo Magaz <pmagaz@atsistemas.com>
2017-08-04 20:13:15 +0200

    minor changes

--- src/app/containers/Blog/selectors/index.js
+++ src/app/containers/Blog/selectors/index.js
@@ -7,11 +7,11 @@ export const paginationState = state => state.Blog.pagination;
 
 export const postsSelector = createSelector(
   postsState,
-  posts => {
-    return posts.map(post => {
+  posts => (
+    posts.map(post => {
       const reg = new RegExp(`^(.+?)${SiteConf.postOpeningSplitChar}`);
       const opening = reg.exec(post.html); 
       if (opening) post.opening = opening[1];
       return post;
-    });
-  });
+    })
+  ));

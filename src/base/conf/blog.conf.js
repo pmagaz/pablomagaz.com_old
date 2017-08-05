f36b6486be8a74dbb531c0d8eb6d5e272bad9eca
Pablo Magaz <pmagaz@atsistemas.com>
2017-08-05 19:06:47 +0200

    Fix conf

--- src/base/conf/blog.conf.js
+++ src/base/conf/blog.conf.js
@@ -10,39 +10,41 @@ const postOpeningChars= 46;
 const codeHighlightDelay = 300;
 const postOpeningSplitChar = '@@@';
 
+let HostName;
 let SiteUrl;
 let BlogUrl;
 let GhostUrl;
 let ImageUrl;
-let baseApiUrl;
+let BaseApiUrl;
 let PostsApiUrl;
 let clientSecret;
 let PostsApi;
 let PostApi;
 
 if (env === 'development') {
+  HostName = 'http://localhost';
   clientSecret = '285ee4eda6c3';
-  SiteUrl = 'http://localhost:8000';
-  BlogUrl = `${SiteUrl}/blog`;
-  GhostUrl = 'http://localhost:2369';
+  SiteUrl = `${ HostName }:8000`;
+  BlogUrl = `${ SiteUrl }/blog`;
+  GhostUrl = `${ HostName }:2369`;
   ImageUrl = GhostUrl;
-  BlogUrl = `${GhostUrl}/blog/`;
-  baseApiUrl = `${GhostUrl}/ghost/api/v0.1/`;
-  PostsApiUrl = `${SiteUrl}/api/posts/`;
-  PostsApi = `${baseApiUrl}posts/?client_id=ghost-frontend&client_secret=${clientSecret}&include=tags&fields=id,uuid,title,slug,html,image,feature_image,tags,updated_at&order=created_at desc&limit=${numPosts}`;
-  PostApi = `${baseApiUrl}posts/slug/:slug/?client_id=ghost-frontend&client_secret=${clientSecret}&include=tags`;
-
+  BlogUrl = `${ GhostUrl }/blog/`;
+  BaseApiUrl = `${ GhostUrl }/ghost/api/v0.1/`;
+  PostsApiUrl = `${ SiteUrl }/api/posts/`;
+  PostsApi = `${ BaseApiUrl }posts/?client_id=ghost-frontend&client_secret=${clientSecret}&include=tags&fields=id,uuid,title,slug,html,image,feature_image,tags,updated_at&order=created_at desc&limit=${numPosts}`;
+  PostApi = `${ BaseApiUrl }posts/slug/:slug/?client_id=ghost-frontend&client_secret=${clientSecret}&include=tags`;
 } else {
+  HostName = 'http://178.62.245.4';
   clientSecret = 'eeb1f09c6698';
-  SiteUrl = 'http://178.62.245.4';
-  BlogUrl = `${SiteUrl}/blog`;
-  GhostUrl = 'http://178.62.245.4:2369';
+  SiteUrl = `${ HostName }`;
+  BlogUrl = `${ SiteUrl }/blog`;
+  GhostUrl = `${ HostName }:2369`;
   ImageUrl = GhostUrl;
-  BlogUrl = `${GhostUrl}/blog/`;
-  baseApiUrl = `${GhostUrl}/ghost/api/v0.1/`;
-  PostsApiUrl = `${SiteUrl}/api/posts/`;
-  PostsApi = `${baseApiUrl}?client_id=ghost-frontend&client_secret=${clientSecret}&include=tags&fields=id,uuid,title,slug,html,image,feature_image,tags,updated_at&order=created_at desc&limit=${numPosts}`;
-  PostApi = `${baseApiUrl}slug/:slug/?client_id=ghost-frontend&client_secret=${clientSecret}&include=tags`;
+  BlogUrl = `${ GhostUrl }/blog/`;
+  BaseApiUrl = `${ GhostUrl }/ghost/api/v0.1/`;
+  PostsApiUrl = `${ SiteUrl }/api/posts/`;
+  PostsApi = `${ BaseApiUrl }posts/?client_id=ghost-frontend&client_secret=${clientSecret}&include=tags&fields=id,uuid,title,slug,html,image,feature_image,tags,updated_at&order=created_at desc&limit=${numPosts}`;
+  PostApi = `${ BaseApiUrl }posts/slug/:slug/?client_id=ghost-frontend&client_secret=${clientSecret}&include=tags`;
 } 
 
 export function getPostUrl(slug) {

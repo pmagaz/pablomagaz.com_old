15bf13d000fa5776761a4dc5f80c9a3a1cf9a0ed
Pablo Magaz <pmagaz@atsistemas.com>
2017-08-06 13:55:10 +0200

    Remove loading

--- src/app/containers/Post/components/PostContent/index.jsx
+++ src/app/containers/Post/components/PostContent/index.jsx
@@ -55,7 +55,7 @@ export class PostContent extends Component {
         <PostDate
           date={ post.created_at }
         />
-        { content }
+        { ReactHtmlParser(post.html) }
       </div>
     </div>
     );

60edda75526862064194073781a6bf1a0ea09f9a
Pablo Magaz <pmagaz@atsistemas.com>
2017-08-09 22:19:18 +0200

    remove httpserver

--- server/HttpServer.js
+++ server/HttpServer.js
@@ -1,11 +1,8 @@
-import * as http from 'http';
-
 import base from 'base/';
 import envConf from './enviroment';
 
 const HttpServer = app => {
-  const httpServer = http.createServer(app);
-  httpServer.listen(envConf.port, (err) => {
+  app.listen(envConf.port, (err) => {
     if (err) return base.console.error(`${err}`);
     base.console.success(`HTTP Server up on http://localhost:${envConf.port}`);
   });

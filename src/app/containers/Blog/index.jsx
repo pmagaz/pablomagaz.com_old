9151e6cfbf40afccc75d03c171d60799e2a059a7
Pablo Magaz <pmagaz@atsistemas.com>
2017-08-14 00:46:03 +0200

    Update logo

--- src/app/containers/Blog/index.jsx
+++ src/app/containers/Blog/index.jsx
@@ -58,7 +58,7 @@ class Blog extends Component {
           <header className={ styles.titleBlog }>
             <div className={ styles.blogTitleWrap }>
             <div className={ blogTitleStyle } > 
-             <img src="assets/images/LogoBlog.png" height="78" />
+             <img src="assets/images/LogoBlog.png" height="84" />
               <h1>
                 { SiteConf.BlogTitle.toUpperCase() }
               </h1>

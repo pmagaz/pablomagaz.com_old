0566a46d60122388749d26e154eee2581b331eea
Pablo Magaz <pmagaz@atsistemas.com>
2017-08-25 10:56:13 +0200

    Clean postImage

--- src/app/components/PostImage/index.jsx
+++ src/app/components/PostImage/index.jsx
@@ -1,9 +1,7 @@
-import ReactDOM from 'react-dom';
-import React, { Component }  from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
-import classNames from 'classnames/bind';
-import CSSTransitionGroup from 'react-addons-css-transition-group';
 import Lazyload from 'react-lazyload';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 import { context } from 'base';
 import styles from './styles.css';
@@ -14,54 +12,21 @@ const propTypes= {
   width: PropTypes.string.isRequired
 };
 
-export class PostImage extends Component {
-
-  constructor(props) {
-    super(props);
-    this.imageRef;
-    this.state = {loaded: false};
-  }
-
-  componentDidMount() {
-    /*
-    const imgTag = ReactDOM.findDOMNode(this.imageRef);
-    const img = new window.Image();
-    img.onload = this.onImageLoad.bind(this);
-    img.src = imgTag.getAttribute('src');
-    */
-  }
-
-  onImageLoad() {
-    this.setState({ loaded: true });
-  }
-
-  render() {
-    const { src, alt, width } = this.props;
-    const cx = classNames.bind(styles);
-
-    const imageStyle = cx({
-      'image': true,
-      //'fade-appear': 
-      //'imageAnim': this.state.loaded && context.client ? true : false 
-    });
-
-    return (
-      <div className = { styles.imageWrapper } >
-        <Lazyload throttle={200} height={120} offset={400}>
-          <CSSTransitionGroup
-            key="1"
-            transitionName="fade"
-            transitionAppear={ true }
-            transitionAppearTimeout={500}
-            transitionEnter={false}
-            transitionLeave={false}>
-            <img className={ imageStyle } src={ src } alt={ alt } width={ width } />
-            </CSSTransitionGroup>
-          </Lazyload>
-      </div>
-    );
-  }
-}
+const PostImage = ({ src, alt, width }) => (
+  <div className = { styles.imageWrapper } >
+    <Lazyload throttle={200} height={120} offset={400}>
+      <CSSTransitionGroup
+        key="1"
+        transitionName="fade"
+        transitionAppear={ true }
+        transitionAppearTimeout={500}
+        transitionEnter={false}
+        transitionLeave={false}>
+        <img className={ styles.image } src={ src } alt={ alt } width={ width } />
+        </CSSTransitionGroup>
+      </Lazyload>
+  </div>
+);
 
 PostImage.propTypes = propTypes;
 

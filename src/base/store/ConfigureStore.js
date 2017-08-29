e3c39e76abb6d2a6d999b2dc0ee2df561c4a808a
Pablo Magaz <pmagaz@atsistemas.com>
2017-08-29 18:20:12 +0200

    Style changes

--- src/base/store/ConfigureStore.js
+++ src/base/store/ConfigureStore.js
@@ -8,9 +8,16 @@ import rootReducer from '../reducers';
 function configureStore(history, initialState) {
 
   let middleware;
+  const loggerMiddleware = store => next => action => {
+    // Mostramos en la consola el type de la accción y el timestamp
+    console.log(`Action dispatched: ${action.type}, Time: ${+new Date()}`);
+    // Devolvemos la acción para que continue el flujo habitual
+    return next(action);
+  };
 
   if (base.env === 'development') {
     middleware = applyMiddleware(
+      loggerMiddleware,
       reduxReqMiddleware(),
       createLogger({ level: 'info', collapsed: true }),
     );
@@ -21,7 +28,7 @@ function configureStore(history, initialState) {
   }
 
   const enhancer = compose(middleware);
-  const store = createStore(rootReducer, initialState, enhancer);
+  const store = createStore(rootReducer, initialState, middleware);
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {

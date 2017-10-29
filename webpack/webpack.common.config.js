e8595d2c1d7d584665d4e84cd0a38298d12e443a
Pablo Magaz <pmagaz@atsistemas.com>
2017-10-29 17:01:57 +0100

    Remove lazy

--- webpack/webpack.common.config.js
+++ webpack/webpack.common.config.js
@@ -28,7 +28,6 @@ export const entry = {
     'react-share',
     'react-router',
     'babel-polyfill',
-    'react-lazyload',
     'isomorphic-fetch',
     'html-react-parser',
     'react-router-scroll',

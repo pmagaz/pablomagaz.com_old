ad8932dcfe276627cb363b59f02bb90236f1e23c
Pablo Magaz <pmagaz@atsistemas.com>
2017-10-29 17:03:18 +0100

    Remove console

--- src/app/components/Header/index.jsx
+++ src/app/components/Header/index.jsx
@@ -25,7 +25,6 @@ class Header extends Component {
     const maxScroll = 112;
     // FIX
     const scrollTop = document.scrollingElement.scrollTop || document.documentElement.scrollTop;
-    console.log(1111, scrollTop);
     if (scrollTop >= maxScroll && !this.state.collapsed) {
       this.setState({ collapsed: true, scrollTop });
     } 

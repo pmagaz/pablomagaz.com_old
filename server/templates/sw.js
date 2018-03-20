2b5979a7489b6e001851a24eef0d4d31bc0595c0
Pablo Magaz <pmagaz@atsistemas.com>
2018-03-20 18:44:11 +0100

    Update siteConf

--- server/templates/sw.js
+++ server/templates/sw.js
@@ -3,7 +3,7 @@ import { SiteConf } from 'base'
 export default `<script>
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('http://localhost:8000/serviceWorker.js').then(function(registration) {
+    navigator.serviceWorker.register('${ SiteConf.SiteUrl }/serviceWorker.js').then(function(registration) {
       console.log('ServiceWorker Ok')
     }, function(err) {
       console.log('ServiceWorker Ko')
--- src/base/serviceWorkers/index.js
+++ src/base/serviceWorkers/index.js
@@ -56,3 +56,25 @@ self.addEventListener('activate', function(event) {
     })
   )
 })
+
+
+self.addEventListener('push', function(e) {
+  let options = {
+    body: 'This notification was generated from a push!',
+    icon: 'images/example.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    },
+    actions: [
+      {action: 'explore', title: 'Explore this new world',
+        icon: 'images/checkmark.png'},
+      {action: 'close', title: 'Close',
+        icon: 'images/xmark.png'},
+    ]
+  }
+  e.waitUntil(
+    self.registration.showNotification('Hello world!', options)
+  )
+})

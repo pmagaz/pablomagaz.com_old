f28a7790ca071ff3af269b36b5d9ed124129016f
pmagaz <magazpablo@gmail.com>
2018-08-04 10:57:18 +0200

    fix worng path

--- server/templates/sw.js
+++ server/templates/sw.js
@@ -1,6 +1,5 @@
-import { env } from 'base'
 import { SiteConf } from 'base'
 
-const  template = `<script src="${ SiteConf.SiteUrl }registerSwV1.js"></script>`
+const  template = `<script src="${ SiteConf.SiteUrl }/registerSwV1.js"></script>`
 
 export default template

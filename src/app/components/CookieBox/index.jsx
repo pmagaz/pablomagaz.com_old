551fd6aadf226d1f88c5fe3d0a03b45f6c7d9d0b
pmagaz <magazpablo@gmail.com>
2018-09-02 19:46:16 +0200

    Add cookie stuff

---
+++ src/app/components/CookieBox/index.jsx
@@ -0,0 +1,53 @@
+import React, { Component  }  from 'react'
+import { pure } from 'recompose'
+import classNames from 'classnames/bind'
+
+import { setCookie, getCookie } from 'base'
+import styles from './styles.css'
+
+class CookieBox extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { showCookieBox : false, faded: false }
+    this.setCookie.bind(this)
+    this.cookieName = 'ElBlogIsomorficoAceptedCookies'
+  }
+
+  componentDidMount() {
+    if (!getCookie(this.cookieName)) this.setState({ showCookieBox: true })
+  }
+
+  setCookie() {
+    setCookie(this.cookieName, 1 ,'Wed, 18 Dec 2023 12:00:00 GMT','/')
+    this.setState({ faded: true })
+  }
+
+  render() {
+    const cx = classNames.bind(styles)
+    const cookieBoxStyle = cx({
+      cookieBox: true,
+      BoxAnim: this.state.faded 
+    })
+
+    const cookieBox = (
+      <div className={ cookieBoxStyle  }>
+        <span className={ styles.cookieBoxMessage  }>
+          Este sitio usa cookies para analíticas y minado de criptomonedas. Es coña, solo para analíticas. 
+          <a aria-label="learn more about cookies" role="button" tabIndex="0" href="https://cookiesandyou.com" target="_blank" rel="noopener noreferrer">Más info</a>
+        </span>
+        <button aria-label="dismiss cookie message" role="button" tabIndex="0" onClick={ () => this.setCookie() }>Entendido</button>
+      </div>
+    ) 
+
+    const showCookieBox = this.state.showCookieBox ? cookieBox : null
+
+    return (
+      <div>
+        { showCookieBox }
+      </div>
+    )
+  }
+}
+
+export default pure(CookieBox)

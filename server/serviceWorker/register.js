e2e1b487ae3484e39cad9401dedc06035d1d1697
pmagaz <magazpablo@gmail.com>
2018-09-02 20:24:52 +0200

    Increase register web push timeout time

--- server/serviceWorker/register.js
+++ server/serviceWorker/register.js
@@ -34,7 +34,7 @@ const webpushSubscribe = swRegistration => {
         }
       })
       .catch(err => console.log('No subscription!', err))
-  ), 8000)
+  ), 8500)
 }
 
 if ('serviceWorker' in navigator) {
@@ -46,7 +46,7 @@ if ('serviceWorker' in navigator) {
   })
 }
 
-const urlBase64ToUint8Array = base64String => {
+function urlBase64ToUint8Array(base64String) {
   const padding = "=".repeat((4 - base64String.length % 4) % 4)
   const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/")
   const rawData = window.atob(base64)

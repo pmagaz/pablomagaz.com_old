9fd5374735ab9ec142493637c01a9c8d2a7b31e5
Magaz Merino <magazpablo@gmail.com>
2018-12-22 12:10:38 +0100

    update sw

--- src/base/shared/regenerateSw.js
+++ src/base/shared/regenerateSw.js
@@ -6,15 +6,14 @@ const Env = require('./Env').default
 const isProd = (Env !== 'development') ? true : false
 const swPath = path.resolve(__dirname, '../../../server/serviceWorker/serviceWorker.js')
 const assetsPath = path.resolve(__dirname, '../../../dist/webpack-assets.json')
-
 if (fs.fileExists(swPath) && fs.fileExists(assetsPath)) {
   const content = fs.readLines(swPath, { encoding: 'utf-8' })
   const assetsContent = JSON.parse(fs.readFile(assetsPath, { encoding: 'utf-8' }))
 
   content[15] = `  'https://pablomagaz.com/blog',`
-  content[16] = `  '/assets${ assetsContent.app.js }',`
-  content[17] = isProd ? `  '/assets${ assetsContent.vendor.js }',` : ''
-  content[18] = isProd ? `  '/assets${ assetsContent.app.css }'` : ''
+  content[16] = `  'https://pablomagaz.com/assets${ assetsContent.app.js }',`
+  content[17] = isProd ? `  'https://pablomagaz.com/assets${ assetsContent.vendor.js }',` : ''
+  content[18] = isProd ? `  'https://pablomagaz.com/assets${ assetsContent.app.css }'` : ''
   const fileContent = content.map(line =>  line + '\n')
   fs.writeFile(swPath, fileContent.join(''))
 }

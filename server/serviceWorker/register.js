2ce825242f7eed60f5d0f9c249c30c48e305873e
Magaz Merino <magazpablo@gmail.com>
2019-04-05 19:20:35 +0200

    Update register sw

--- server/serviceWorker/register.js
+++ server/serviceWorker/register.js
@@ -5,36 +5,18 @@ const permissionDelay = 8000
 const registerUrl = 'https://pablomagaz.com/webpush/register'
 const serviceWorkerUrl = 'https://pablomagaz.com/serviceWorker.js'
 const publicVapidKey = 'BFdszVeNLXOP_BtqQn1o4-g-pV4BMMFHjrkKKn9OSDqiHVUp52GIGw4HEKJv2jpGiPGkaIpFyHk8zZv93J6-bc8'
-const applicationServerKey = urlBase64ToUint8Array(publicVapidKey)
 
-const register = async () => {
-  if ('serviceWorker' in navigator) {
-    const swRegistration = await registerServiceWorker()
-    await registerSubscription(swRegistration)
-  } else throw new Error('ServiceWorkers are not supported by your browser!')
+const urlBase64ToUint8Array = (base64String) => {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
+  const rawData = atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+
+  for (let i = 0; i < rawData.length; i += 1) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
 }
-
-const registerServiceWorker = async () => {
-  return await navigator.serviceWorker.register(serviceWorkerUrl)
-}
-
-const registerSubscription = swRegistration =>
-  new Promise((resolve, reject) => {
-    setTimeout(async () => {
-      await window.Notification.requestPermission()
-      const subscribed = await swRegistration.pushManager.getSubscription()
-      if (!subscribed) {
-        const subscription = await swRegistration.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey
-        })
-        const saved = await saveSubscription(subscription)
-        if (saved) resolve(saved)
-        else reject(new Error('Subscription not saved!'))
-      }
-      resolve(subscribed)
-    }, permissionDelay)
-  })
 
 const saveSubscription = async subscription => {
   const res = await fetch(registerUrl, {
@@ -48,16 +30,33 @@ const saveSubscription = async subscription => {
   return res.status === 200 ? res.json() : false
 }
 
-function urlBase64ToUint8Array(base64String) {
-  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
-  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
-  const rawData = atob(base64)
-  const outputArray = new Uint8Array(rawData.length)
+const generateSubscription = swRegistration =>
+  new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      await window.Notification.requestPermission()
+      const subscribed = await swRegistration.pushManager.getSubscription()
+      if (!subscribed) {
+        const subscription = await swRegistration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+        })
+        const saved = await saveSubscription(subscription)
+        if (saved) resolve(saved)
+        else reject(new Error('Subscription not saved!'))
+      }
+      resolve(subscribed)
+    }, permissionDelay)
+  })
 
-  for (let i = 0; i < rawData.length; i += 1) {
-    outputArray[i] = rawData.charCodeAt(i)
-  }
-  return outputArray
+const registerServiceWorker = async () => {
+  return await navigator.serviceWorker.register(serviceWorkerUrl)
+}
+
+const register = async () => {
+  if ('serviceWorker' in navigator) {
+    const swRegistration = await registerServiceWorker()
+    await generateSubscription(swRegistration)
+  } else throw new Error('ServiceWorkers are not supported by your browser!')
 }
 
 register()
